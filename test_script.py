9cf0107275e5845cb6d4d5681476c10bde1fed78
Dorian Deneuville <dorian.deneuville@gmail.com>
2020-08-04 23:53:24 +0200

    modifier les tests

--- test_script.py
+++ test_script.py
@@ -6,17 +6,21 @@ d = {
 }
 
 port2 = Portfolio(d,
-                  cov=pd.DataFrame(np.array([[1.2, -0.1],[-0.1,0.3]]), columns=d.keys(), index=d.keys()),
-                  kappa=0.1)
+                  cov=pd.DataFrame(
+                      np.array([[0.04, 0], [0, 0.01]]), columns=d.keys(), index=d.keys()),
+                  kappa=1)
+
+port2.imp_kap()
+
 
 v = Views()
 df = pd.DataFrame(
-{
-    'fixed_income': [0, 1, 1],
-    'equities': [1, 0, 0],
-    'r': [0.05, 0.01, 0.01],
-    'c': [1.0, 1.0, 1.5]
-})
+    {
+        'fixed_income': [0, 1],
+        'equities': [1, 0],
+        'r': [0.05, 0.01],
+        'c': [1.0, 1.0]
+    })
 v.add_views(df)
 
 
@@ -24,7 +28,6 @@ problem = PortfolioProblem(port2, v)
 # for k in range(len(v)):
 #     problem.post_ret100_k(k, )
 
+# port2.display()
 
-
-
-
+problem.post_ret100_k(0)
